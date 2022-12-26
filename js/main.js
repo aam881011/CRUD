6453202ab82609fc0e8ebb5fda01425ed8a51fb1
Mohamed Dahy Mohamed <72628743+aam881011@users.noreply.github.com>
2022-12-26 09:29:18 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ let count = document.getElementById("count");
 let category = document.getElementById("category");
 let submit = document.getElementById("submit");
 
-let mood = "create";
+let mood = "creat";
 let tmp;
 
 // get total
